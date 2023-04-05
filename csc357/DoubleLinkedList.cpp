06bef2d141779be0e004f11221206c2eb7751f48
Saumitra Tiwari <satiwari@calpoly.edu>
2023-04-05 11:54:40 -0700

    Linked list created working on the menu component

---
+++ csc357/DoubleLinkedList.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+struct listElement{
+    char text[1000];    //char array to store the string
+    listElement * next, *prev; //pointer of the node to store the next and prev links
+};
+
+
+listElement * head = NULL; //global head pointer to point to the nodes
+
+void newNode(char value[1000]);
+void printList();
+
+
+void newNode(char value[1000]){
+    bool firstNode = false;
+    listElement * temp; //temp pointer to hold the address of the new node
+    temp = new listElement; //creating a new node on the heap
+    if(head == NULL){
+        head = temp;    //head points to the new node
+        firstNode = true;
+    }
+    else{
+        firstNode = false; // to prevent linking of the node to itself;
+    }
+
+    strcpy(temp->text,value);   //copy the entire string from array value to array text.
+    temp->next = NULL;
+    temp->prev = NULL;
+
+    //temp1 is declared to traverse through the list
+    listElement *temp1 = head;
+    if(firstNode==false){
+        while(temp1->next != NULL){   //checking if the next node is present
+            temp1 = temp1->next;    //if it is setting the temp1 to the next node
+        }
+        temp1->next = temp;  //linking the last node to new node
+        temp->prev = temp1; //linking the new node prev to last node
+    }
+}
+
+void printList(){
+    listElement * temp = head;  //temp pointer to traverse through the list
+    if(temp == NULL){
+        cout<<"There are no List elements."<<endl;
+    }
+    else{
+        while(temp != NULL){    //print until temp is NULL
+            cout<<temp->text<<" ";  //dereferncing itself and getting the value of the current node its on
+            temp = temp->next;  //after printing the current value of the node move temp to the next node.
+        }
+
+        cout<<endl; //to exit it to new line.
+    }
+}
+
+
+int main(){
+    //char text[1000];
+    //strcpy(text,"Dhruv");
+
+    newNode("saumitra");
+    newNode("Dhruv");
+    newNode("Hello");
+    newNode("DREW");
+
+    printList();
+
+    return 0;
+}
