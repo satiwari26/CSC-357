d01b413afa569aa96da3757c2112b8a5a80412fd
Saumitra Tiwari <satiwari@calpoly.edu>
2023-04-29 21:49:09 -0700

    figured out the buffer and pipe issue

--- csc357/signals.cpp
+++ csc357/signals.cpp
@@ -5,15 +5,22 @@
 #include<signal.h>
 #include <iostream>
 #include<time.h>
+#include<sys/mman.h>
 #include <sys/wait.h>
 using namespace std;
 
 int fd[2];
 int sigFlagChild =0;
 int sigFlagParent =0;
+    char text[1000];
+    char activity[30];
 //creating a signal handler function
 void signalhandlerParent(int sig){
+    int save_stdin = dup(STDIN_FILENO);
     dup2(fd[0], STDIN_FILENO);  //overwriting the stdin with fd[0]
+    read(STDIN_FILENO, text,21);    //read the value in from the other end of the pipe
+        dup2(save_stdin,STDIN_FILENO);
+        cout<<text<<endl;
     sigFlagParent = 1;
 }
 
@@ -26,23 +33,23 @@ int main(){
     //creating a pipe function to communicate b/w child and parent
     pipe(fd);
     int parentPID = getpid();
-    char text[1000];
-    char activity[30];
+    int *childPID = (int *)mmap(NULL,sizeof(int),PROT_READ|PROT_WRITE,MAP_SHARED|MAP_ANONYMOUS,-1,0);
 
     //forking into parent and child processes
     if(fork()==0){
+            *childPID = getpid();
+            signal(SIGUSR1,signalhandlerChild);
            while(true){
             time_t sTime = time(NULL); //start time
             int eTime = 0;
 
-            while(eTime < 2){
-                signal(SIGUSR1,signalhandlerChild);
+            while(eTime < 5){
                 eTime = time(NULL)-sTime;
             }
             if(sigFlagChild !=1){
                 close(fd[0]);   //writing end of the pipe
                 kill(parentPID,SIGUSR1);    //passing the signal to the parent
-                write(fd[1],"nothing", 21);
+                write(fd[1],"user is Inactive!", 21);
                 close(fd[1]);
             }
             sigFlagChild = 0;
@@ -51,27 +58,13 @@ int main(){
     }
     else{
         close(fd[1]); //reading end of the pipe
-        int save_stdin = dup(STDIN_FILENO);
+        
+        signal(SIGUSR1,signalhandlerParent);  //redirecting the signal from the parent.
 
         //loop through to countinously read the data in from the stdin
         while(true){
-            signal(SIGUSR1,signalhandlerParent);  //redirecting the signal from the parent.
-            if(sigFlagParent ==1){
-            read(STDIN_FILENO, text,21);    //read the value in from the other end of the pipe
-            cout<<text<<endl;
-            dup2(save_stdin,STDIN_FILENO);
-            }
-            else{
-            scanf("%s",text); //get the text value from the user
-            }
-            //signal(SIGUSR1,signalhandlerParent);  //redirecting the signal from the parent.
-
-            // if(sigFlagParent ==1){
-            // read(STDIN_FILENO, activity, 21);    //read the value in from the other end of the pipe
-            // cout<<activity<<endl;
-            // dup2(save_stdin,STDIN_FILENO);
-            // }
-            sigFlagParent =0;
+             scanf("%s",text); //get the text value from the user
+            // kill(*childPID, SIGUSR1);
         }
         wait(0);
     }
