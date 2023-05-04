9dec60d36845d4caf84bee5ef21e673e2ecd242e
Saumitra Tiwari <satiwari@calpoly.edu>
2023-05-04 12:39:35 -0700

    changes made to the test file

--- csc357/test.c
+++ csc357/test.c
@@ -5,9 +5,15 @@
 
 int main(){
 
-    char text[20] = "hello folks!";
-    strcat(text," hi!");
-    printf("%s",text);
+    char text[20] = "hello world!";
+    char text2[20] = "my name ";
+
+    char **p;
+     *p = text;
+    //   *(p+1) = text2;
+
+     printf("%s \n",*p);
+      //printf("%s \n",*(p+1));
 
     return 0;
 }
