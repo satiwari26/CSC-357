585066e1a6c737ab68687be7f136d87f95cc0f01
Saumitra Tiwari <satiwari@calpoly.edu>
2023-05-08 15:13:24 -0700

    conditional allocation of memory if no heap was assigned, first 24 bytes of data is heap info

--- csc357/myMalloc.cpp
+++ csc357/myMalloc.cpp
@@ -1,8 +1,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include<stdio.h>
+#include <iostream>
+using namespace std;
 
-typedef unsigned int BYTE; //for 32 bit system, guessing the size is 4 byte
+typedef unsigned char BYTE; 
 
 struct chunkinfo
 {
@@ -11,8 +13,45 @@ int inuse; //is it free? 0 or occupied? 1
 BYTE *next,*prev;//address of next and prev chunk
 };
 
+void *startOfHeap = NULL;
+void *EndOfHeap = NULL;
 
-int main(){
+void *myMalloc(int size){    //accepts the size of the memory to be allocated
+    void *addr;
+    int actualSize = size+ sizeof(chunkinfo);
+    int numPages = 0;
+    if(startOfHeap==NULL){  //if no heap then create the chunk requested by the user
+        if(actualSize<=4096){
+            addr = sbrk(4096);
+            //to store the info of the chunk in the dynamic allocated mem, have to typeCase it
+            chunkinfo *ptr = (chunkinfo *)addr;
+            //assign the chunk info here. the size of chunk info is 24 bytes
+            ptr->inuse = 0;     //since only being allocated and not in_use
+            ptr->size = actualSize;
+            ptr->next = NULL;
+            ptr->prev = NULL;
+            //end of the chunk info
+
+            //set the start,end of the heap to this chunk.
+            startOfHeap = addr;
+            EndOfHeap = addr;
+            //return the ptr with offset of 24 bytes
+            return(addr+sizeof(chunkinfo));
+        }
+        else if((actualSize%4096)==0){
+            numPages = (actualSize/4096);
+        }
+        else{
+            numPages = (actualSize/4096)+1;
+            
+        }
+    }
+
+}
+
+
+int main(){ 
+
 
     return 0;
 }
