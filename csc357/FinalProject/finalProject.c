22fac0ebb45663866c8d0a46cd39294cd78df3c3
Saumitra Tiwari <satiwari@calpoly.edu>
2023-06-06 11:04:43 -0700

    started working on the final Project

---
+++ csc357/FinalProject/finalProject.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int main(){
+
+    
+
+    return 0;
+}
